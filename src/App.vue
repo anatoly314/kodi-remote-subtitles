c37017803fc74400bc5c621d530c4822e52a5fd9
Anatoly Tarnavsky <anatolyt@gmail.com>
2020-06-24 12:33:09 +0300

    Dynamic public path to make it compatible with github static pages

--- src/App.vue
+++ src/App.vue
@@ -31,8 +31,6 @@ export default {
   @import '../node_modules/vue-virtual-scroller/dist/vue-virtual-scroller.css';
   @import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900');
   @import url('https://use.fontawesome.com/releases/v5.0.13/css/all.css');
-  @import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
-  @import "../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css";
 
   html,
   body,
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/kodi-remote-subtitles/' : '/',
     configureWebpack: {
         plugins: [
             new VuetifyLoaderPlugin()

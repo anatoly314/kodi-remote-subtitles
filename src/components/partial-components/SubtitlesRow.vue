ca0b991333d90c1ab4c61e5c309520cebcf6d441
Anatoly Tarnavsky <anatolyt@gmail.com>
2020-06-29 14:24:59 +0300

    Added support to copy text

--- src/components/partial-components/SubtitlesRow.vue
+++ src/components/partial-components/SubtitlesRow.vue
@@ -5,6 +5,11 @@
         <div class="time-container" v-if="displaySubtitlesTime">
             <div>{{toSrtTime(rowData.start)}}</div>
             <div>{{toSrtTime(rowData.end)}}</div>
+            <v-btn class="mx-2"
+                   @click="copyToClipboard(rowData.text)"
+                   icon x-small color="primary">
+                <v-icon>fa-copy</v-icon>
+            </v-btn>
         </div>
         <div class="text-container">
             <span v-html="rowData.text"></span>
@@ -36,6 +41,52 @@
             ...mapActions('kodi',[
                 'MOVE_TO_SPECIFIC_TIME'
             ]),
+            copyToClipboard (text) {
+                const self = this;
+                function fallbackCopyTextToClipboard(text) {
+                    const textArea = document.createElement("textarea");
+                    textArea.value = text;
+
+                    // Avoid scrolling to bottom
+                    textArea.style.top = "0";
+                    textArea.style.left = "0";
+                    textArea.style.position = "fixed";
+
+                    document.body.appendChild(textArea);
+                    textArea.focus();
+                    textArea.select();
+
+                    try {
+                        document.execCommand('copy');
+                        self.$bus.$emit('show-notification', {
+                            type: 'info',
+                            text: 'Text successfully copied'
+                        });
+                    } catch (err) {
+                        self.$bus.$emit('show-notification', {
+                            type: 'error',
+                            text: `Error copying text ${err.toString()}`
+                        });
+                    }
+
+                    document.body.removeChild(textArea);
+                }
+                if (!navigator.clipboard) {
+                    fallbackCopyTextToClipboard(text);
+                    return;
+                }
+                navigator.clipboard.writeText(text).then(function() {
+                    self.$bus.$emit('show-notification', {
+                        type: 'info',
+                        text: 'Text successfully copied'
+                    });
+                }, function(err) {
+                    self.$bus.$emit('show-notification', {
+                        type: 'error',
+                        text: `Error copying text ${err.toString()}`
+                    });
+                });
+            },
             toSrtTime: toSrtTime
         },
         components: {

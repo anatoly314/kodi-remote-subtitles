346dbf604903293eb1ad44aee4381d6f80b3bceb
Anatoly Tarnavsky <anatolyt@gmail.com>
2020-08-01 11:46:28 +0300

    Some bundle optimization

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,23 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const webpack = require('webpack');
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+const StatsPlugin = require('stats-webpack-plugin');
+
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
     configureWebpack: {
+        devtool: 'source-map',
         plugins: [
-            new VuetifyLoaderPlugin()
-        ],
+            new VuetifyLoaderPlugin(),
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+            new DuplicatePackageCheckerPlugin(),
+            new StatsPlugin('stats.json')
+        ]
     },
+    pluginOptions: {
+        webpackBundleAnalyzer: {
+            openAnalyzer: false
+        }
+    }
 }
